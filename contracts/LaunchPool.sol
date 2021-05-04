08624371e7ddf03745b7c33154468e1d2dd07b92
green-john <andresfeliperu@gmail.com>
2021-05-04 10:46:24 +0200

    add new stuffs'

--- contracts/LaunchPool.sol
+++ contracts/LaunchPool.sol
@@ -5,43 +5,98 @@ import "@openzeppelin/contracts/utils/escrow/Escrow.sol";
 import "./interfaces/IERC20Minimal.sol";
 import "hardhat/console.sol";
 
+struct Stake {
+    /** Starts with 1. Determines the position in line as well as the id. */
+    uint256 id;
+    /** Account who added this stake. */
+    address staker;
+    /** Token being staked. For now only ERC20 */
+    address token;
+    /** Amount staked */
+    uint256 amount;
+}
+
 contract LaunchPool {
+    enum Stages {AcceptingStakes, AcceptingCommitments, Funded, Closed};
+
+    /** All pools start in this state  */
+    Stages public stage = Stages.AcceptingStakes;
+
+    uint256 public poolStartTime = block.timestamp;
+
     string public name;
-    uint256 public maxCommitment;
-    uint256 public minCommitment;
 
-    uint256 private _endTimestamp;
-    uint256 private _totalStaked;
-    uint256 private _accountsStakeCount;
+    /** How long will this launchpool be valid for.  */
+    uint256 public poolValidDuration;
 
-    mapping(address => mapping(address => uint256)) private _stakes;
+    /** @dev the minimum amount an offer should gather in order to be redeemable */
+    uint256 public minOfferAmount;
+
+    /** @dev the maximum amount an offer can get. All stakes that are above this value
+     * will not be used and will be returned to the account that staked them initially.
+     * TODO: what do we do when stakedSoFar + currentStake > maxOfferAmount. We could clip
+     * the stake and return the remainder.
+     */
+    uint256 public maxOfferAmount;
+
+    /** When did the offer start? i.e., when were people allowed to commit? */
+    uint256 public offerStartTime;
+
+    /** Time the offer is valid for in ms */
+    uint256 public offerValidDuration;
+
+    /** If this value is empty, we assume people cannot commit  */
+    string public offerUrl;
+
+    uint256 private _placeInLine;
+
+    /** Stakes indexed by id */
+    mapping(uint256 => Stake) private _stakes;
+
+    /** Allowed tokens  */
     mapping(address => bool) private _allowedTokenAddresses;
-    mapping(address => uint256) private _stakesByAccount;
 
-    event Staked(address indexed payee, address indexed token, uint256 amount);
+    /** Commited stakes */
+    mapping(uint256 => bool) private _stakesCommitted;
+
+    event Staked(
+        address indexed payee,
+        address indexed token,
+        uint256 amount,
+        uint256 stakeId
+    );
+
     event Unstaked(
         address indexed payee,
         address indexed token,
-        uint256 amount
+        uint256 amount,
+        uint256 stakeId
+    );
+
+    event Committed(
+        address indexed payee,
+        uint256 indexed stakeId
     );
 
     constructor(
         address[] memory allowedAddresses_,
         string memory _poolName,
-        uint256 _minCommitment,
-        uint256 _maxCommitment,
-        uint256 endTimestamp_
+        uint256 poolValidDuration_,
+        uint256 offerValidDuration_
+        uint256 minOfferAmount_,
+        uint256 maxOfferAmount_
     ) {
-        // Allow at most 3 coins
         require(
             allowedAddresses_.length >= 1 && allowedAddresses_.length <= 3,
             "There must be at least 1 and at most 3 tokens"
         );
         name = _poolName;
-        minCommitment = _minCommitment;
-        maxCommitment = _maxCommitment;
-        _endTimestamp = endTimestamp_;
+        poolValidDuration = poolValidDuration_;
+        offerValidDuration = offerValidDuration_;
+        minOfferAmount = minOfferAmount_;
+        maxOfferAmount = maxOfferAmount_;
 
+        // TOOD on my testing a for loop didn't work here, hence this uglyness.
         _allowedTokenAddresses[allowedAddresses_[0]] = true;
         if (allowedAddresses_.length >= 2) {
             _allowedTokenAddresses[allowedAddresses_[1]] = true;
@@ -52,6 +107,10 @@ contract LaunchPool {
         }
     }
 
+    function _atStage(Stage stage_) private view returns (bool) {
+        return stage_ == stage;
+    }
+
     modifier isTokenAllowed(address _tokenAddr) {
         require(
             _allowedTokenAddresses[_tokenAddr],
@@ -60,16 +119,42 @@ contract LaunchPool {
         _;
     }
 
-    modifier isLaunchPoolOpen() {
-        require(block.timestamp <= _endTimestamp, "LaunchPool is closed");
+    modifier isPoolOpen() {
+        require(
+            (poolStartTime + poolValidDuration <= block.timestamp) &&
+            (!_atStage(Stages.Closed)),
+            "LaunchPool is closed");
         _;
     }
 
-    modifier hasRoomForDeposit(uint256 amount) {
+    modifier canStake() {
         require(
-            _totalStaked + amount <= maxCommitment,
-            "Maximum staked amount exceeded"
+            _atStage(Stages.AcceptingStakes) || _atStage(Stages.AcceptingCommitments),
+            "Not in desired stage"
         );
+        _;
+    }
+
+    modifier canCommit() {
+        require(
+            _atStage(Stages.AcceptingCommitments),
+            "Not in either of desired stages"
+        );
+        _;
+    }
+
+    modifier senderOwnsStake(uint stakeId) {
+        require(
+            _stake[stakeId].staker == msg.sender,
+            "Account not authorized to unstake"
+        );
+        _;
+    }
+
+    modifier isOfferOpen() {
+        require(
+            (offerStartTime + offerValidDuration <= block.timestamp) &&
+            "Offer is closed");
         _;
     }
 
@@ -79,8 +164,8 @@ contract LaunchPool {
     function stake(address token, uint256 amount)
         external
         isTokenAllowed(token)
-        isLaunchPoolOpen
-        hasRoomForDeposit(amount)
+        isPoolOpen
+        canStake
     {
         address payee = msg.sender;
 
@@ -89,39 +174,55 @@ contract LaunchPool {
         }
 
         _stakesByAccount[payee] = _stakesByAccount[payee] + amount;
-        _stakes[payee][token] = _stakes[payee][token] + amount;
         _totalStaked += amount;
+        uint stakeId = ++_placeInLine;
 
-        // If the transfer fails, we revert and don't record the amount.
-        require(
-            IERC20Minimal(token).transferFrom(
-                msg.sender,
-                address(this),
-                amount
-            ),
-            "Did not get the moneys"
-        );
+        // This adds a new stake to _stakes
+        Stake storage s = _stakes[stakeId];
 
-        emit Staked(payee, token, amount);
+        s.id = stakeId;
+        s.staker = payee;
+        s.token = token;
+        s.amount = amount;
+
+        // TODO: call the vault contract
+        // to transfer funds from user.
+
+        emit Staked(payee, token, amount, stakeId);
     }
 
-    function unstake(address token) external isTokenAllowed(token) {
-        uint256 currStake = _stakes[msg.sender][token];
+    function unstake(uint stakeId)
+        external
+        senderOwnsStake(stakeId)
+        isPoolOpen
+        canStake 
+    {
+        require(!_committedStakes[stakeId], "cannot unstake commited stake");
 
-        _totalStaked -= currStake;
-        _stakesByAccount[msg.sender] -= currStake;
-        _stakes[msg.sender][token] = 0;
+        Stake s = _stakes[stakeId];
+        delete _stakes[stakeId];
 
-        if (!_accountHasStaked(msg.sender)) {
-            _accountsStakeCount -= 1;
-        }
+        // TODO: call vault to unstake
+        emit Unstaked(msg.sender, token, currStake, stakeId);
+    }
 
-        require(
-            IERC20Minimal(token).transfer(msg.sender, currStake),
-            "Could not send the moneys"
-        );
+    function setOffer(string storage offerUrl_) isPoolOpen external {
+        // TODO this should only be callable by a sponsor
+        offerStartTime = block.timestamp;
+        offerUrl = offerUrl_;
+        stage = Stages.AcceptingCommitments;
+    }
 
-        emit Unstaked(msg.sender, token, currStake);
+    function commit(uint stakeId)
+        external
+        isPoolOpen
+        isOfferOpen
+        canCommit
+        senderOwnsStake(stakeId)
+    {
+        _committedStakes[stakeId] = true;
+
+        emit Committed(msg.sender, stakeId);
     }
 
     function stakesOf(address payee, address token)

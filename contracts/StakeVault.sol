a23219fbf49e55c2a0661e9c1a5fce4c496c06ba
green-john <andresfeliperu@gmail.com>
2021-05-21 18:51:45 +0500

    Fix StakeVault and tests to accept multiple pools
    Before this, vaults were design to work with only one pool,
    now all vaults have to be scoped by `poolId`. This change
    is done because we want to use the same contract for
    multiple pools.

--- contracts/StakeVault.sol
+++ contracts/StakeVault.sol
@@ -10,43 +10,62 @@ import "hardhat/console.sol";
 ///     When a vault is created, a `withdrawAddress` is set. This address will
 ///     receive the funds once they are withdrawn.
 contract StakeVault is Ownable {
-    address private _poolContract;
+
+    struct PoolDeposit {
+        uint256 _totalDeposited;
+
+        // payee -> token -> deposit
+        mapping(address => mapping(address => uint256)) _deposits;
+
+        // payee -> totalDeposits
+        mapping(address => uint256) _depositsByAccount;
+
+        // payee -> canWithdraw
+        mapping(address => bool) _shouldWithdrawToken;
+
+        // payee -> amount
+        mapping(address => uint256) _amountToWithdraw;
+
+        address[] _tokensToWithdraw;
+    }
+
+    address private _poolTrackerContract;
 
     uint256 public totalDeposited;
 
     /** @dev Holds will be withdrawn to this address. Cannot be changed once the contract is deployed */
     address public withdrawAddress;
 
+    // poolId -> all pool deposits
+    mapping(uint256 => PoolDeposit) private _poolDeposits;
+
     constructor(address withdrawAddress_) {
         withdrawAddress = withdrawAddress_;
     }
 
-    mapping(address => mapping(address => uint256)) private _deposits;
-    mapping(address => uint256) private _depositsByAccount;
-
-    mapping(address => bool) private _shouldWithdrawToken;
-    mapping(address => uint256) private _amountToWithdraw;
-    address[] private _tokensToWithdraw;
-
     modifier calledByPool() {
-        require(_poolContract != address(0), "pool contract not set");
-        require(_poolContract == msg.sender, "sender is not the pool contract");
+        require(_poolTrackerContract != address(0), "pool contract not set");
+        require(_poolTrackerContract == msg.sender, "sender is not the pool contract");
         _;
     }
 
     /// @dev Of the pool contract is not set, none of the methods in this contract work.
     function setPoolContract(address poolContract) public onlyOwner {
-        _poolContract = poolContract;
+        _poolTrackerContract = poolContract;
     }
 
     /// @notice Transfer ERC20 funds from the user to the vault.
     function depositStake(
+        uint256 poolId,
         address token,
         uint256 amount,
         address payee
     ) public calledByPool {
-        _depositsByAccount[payee] = _depositsByAccount[payee] + amount;
-        _deposits[payee][token] = _deposits[payee][token] + amount;
+        PoolDeposit storage pd = _poolDeposits[poolId];
+
+        pd._depositsByAccount[payee] = pd._depositsByAccount[payee] + amount;
+        pd._deposits[payee][token] = pd._deposits[payee][token] + amount;
+        pd._totalDeposited += amount;
         totalDeposited += amount;
 
         // If the transfer fails, we revert and don't record the amount.
@@ -56,83 +75,94 @@ contract StakeVault is Ownable {
         );
 
         // Assert here so we have peace of mind
-        assert(_depositsByAccount[payee] <= totalDeposited);
+        assert(pd._depositsByAccount[payee] <= pd._totalDeposited);
     }
 
     /// @notice Withdraw a stake.
     function withdrawStake(
+        uint256 poolId,
         address token,
         uint256 amount,
         address payee
     ) public calledByPool {
         require(
-            depositsOfByToken(payee, token) >= amount,
+            depositsOfByToken(poolId, payee, token) >= amount,
             "Player has less than requested amount"
         );
 
-        _removeAmount(payee, token, amount);
+        _removeAmount(poolId, payee, token, amount);
 
         require(
             IERC20Minimal(token).transfer(payee, amount),
             "Could not send the moneys"
         );
 
-        assert(_depositsByAccount[payee] <= totalDeposited);
+        assert(_poolDeposits[poolId]._depositsByAccount[payee] <= totalDeposited);
     }
 
-    function depositsOf(address payee) public view returns (uint256) {
-        return _depositsByAccount[payee];
+    function depositsOf(uint256 poolId, address payee) public view returns (uint256) {
+        return _poolDeposits[poolId]._depositsByAccount[payee];
     }
 
-    function depositsOfByToken(address payee, address token)
+    function depositsOfByToken(uint256 poolId, address payee, address token)
         public
         view
         returns (uint256)
     {
-        return _deposits[payee][token];
+        return _poolDeposits[poolId]._deposits[payee][token];
     }
 
     function _removeAmount(
+        uint256 poolId,
         address payee,
         address token,
         uint256 amount
     ) private {
         totalDeposited -= amount;
-        _depositsByAccount[payee] -= amount;
-        _deposits[payee][token] -= amount;
+
+        PoolDeposit storage pd = _poolDeposits[poolId];
+        pd._totalDeposited -= amount;
+        pd._depositsByAccount[payee] -= amount;
+        pd._deposits[payee][token] -= amount;
     }
 
     /// @dev encumbers a token from a payee to be later withdrawn by an account.
     ///     In order words, it marks a certain amount for certain token to
     ///     be sent to the `withdrawAddress`
     function encumber(
+        uint256 poolId,
         address payee,
         address token,
         uint256 amount
     ) public calledByPool {
         require(
-            depositsOfByToken(payee, token) >= amount,
+            depositsOfByToken(poolId, payee, token) >= amount,
             "Not enough tokens to encumber"
         );
 
-        if (!_shouldWithdrawToken[token]) {
-            _shouldWithdrawToken[token] = true;
-            _tokensToWithdraw.push(token);
+        _removeAmount(poolId, payee, token, amount);
+
+        PoolDeposit storage pd = _poolDeposits[poolId];
+
+        if (!pd._shouldWithdrawToken[token]) {
+            pd._shouldWithdrawToken[token] = true;
+            pd._tokensToWithdraw.push(token);
         }
 
-        _removeAmount(payee, token, amount);
-        _amountToWithdraw[token] += amount;
+        pd._amountToWithdraw[token] += amount;
     }
 
-    /// @dev Withdraw the funds to the specified address.
-    function withdraw() public calledByPool {
-        for (int256 i = 0; i < int256(_tokensToWithdraw.length); i++) {
-            address token = _tokensToWithdraw[uint256(i)];
+    /// @dev Withdraw the funds for a pool to the specified address.
+    function withdraw(uint256 poolId) public calledByPool {
+        PoolDeposit storage pd = _poolDeposits[poolId];
+
+        for (int256 i = 0; i < int256(pd._tokensToWithdraw.length); i++) {
+            address token = pd._tokensToWithdraw[uint256(i)];
 
             require(
                 IERC20Minimal(token).transfer(
                     withdrawAddress,
-                    _amountToWithdraw[token]
+                    pd._amountToWithdraw[token]
                 ),
                 "Could not send the moneys"
             );
--- test/vault-test.js
+++ test/vault-test.js
@@ -2,16 +2,18 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Test stake vault", function() {
-  let owner, acc1, acc2, recvFunds, token1, token2, token3, stakeVault;
+  let owner, acc1, acc2, recvFunds, token1, token2, token3, token4, stakeVault;
 
   const initialAmount = 100;
+  const poolId1 = "00";
+  const poolId2 = "01";
 
   async function _mintTokensAndApprove(token, account, amount) {
     await token.mint(account.address, amount);
     await token.connect(account).approve(stakeVault.address, amount);
   }
 
-  beforeEach("create and deploy erc and vault", async () => {
+  beforeEach("create and deploy erc and vault with 1 pool", async () => {
     [ owner, acc1, acc2, recvFunds ] = await ethers.getSigners();
 
     MockERC20 = await ethers.getContractFactory("MockERC20");
@@ -38,94 +40,99 @@ describe("Test stake vault", function() {
     const StakeVault = await ethers.getContractFactory("StakeVault");
     const notInitted = await StakeVault.deploy(recvFunds.address);
 
-    await expect(notInitted.depositStake(token1.address, 10, owner)).to.be.reverted;
+    await expect(notInitted.depositStake(poolId1, token1.address, 10, owner)).to.be.reverted;
   });
 
   it("Nothing in vault", async function() {
-    expect(await stakeVault.depositsOfByToken(owner.address, token1.address)).to.equal(0);
-    expect(await stakeVault.depositsOfByToken(owner.address, token2.address)).to.equal(0);
-    expect(await stakeVault.depositsOfByToken(owner.address, token3.address)).to.equal(0);
+    expect(await stakeVault.depositsOfByToken(poolId1, owner.address, token1.address)).to.equal(0);
+    expect(await stakeVault.depositsOfByToken(poolId2, owner.address, token1.address)).to.equal(0);
+
+    expect(await stakeVault.depositsOfByToken(poolId1, owner.address, token2.address)).to.equal(0);
+    expect(await stakeVault.depositsOfByToken(poolId2, owner.address, token2.address)).to.equal(0);
+
+    expect(await stakeVault.depositsOfByToken(poolId1, owner.address, token3.address)).to.equal(0);
+    expect(await stakeVault.depositsOfByToken(poolId2, owner.address, token3.address)).to.equal(0);
     expect(await stakeVault.totalDeposited()).to.equal(0);
   });
 
   it("Deposit token1 and get amount staked", async function() {
-    await stakeVault.depositStake(token1.address, 10, owner.address);
+    await stakeVault.depositStake(poolId1, token1.address, 10, owner.address);
 
     expect(await token1.balanceOf(owner.address)).to.equal(90);
 
-    expect(await stakeVault.depositsOfByToken(owner.address, token1.address)).to.equal(10);
-    expect(await stakeVault.depositsOf(owner.address)).to.equal(10);
+    expect(await stakeVault.depositsOfByToken(poolId1, owner.address, token1.address)).to.equal(10);
+    expect(await stakeVault.depositsOf(poolId1, owner.address)).to.equal(10);
     expect(await stakeVault.totalDeposited()).to.equal(10);
   });
 
   it("Deposit token1 then withdraw", async function() {
-    await stakeVault.depositStake(token1.address, 10, owner.address);
+    await stakeVault.depositStake(poolId1, token1.address, 10, owner.address);
 
-    expect(await stakeVault.depositsOfByToken(owner.address, token1.address)).to.equal(10);
+    expect(await stakeVault.depositsOfByToken(poolId1, owner.address, token1.address)).to.equal(10);
     expect(await token1.balanceOf(owner.address)).to.equal(90);
 
-    await stakeVault.withdrawStake(token1.address, 5, owner.address);
+    await stakeVault.withdrawStake(poolId1, token1.address, 5, owner.address);
 
-    expect(await stakeVault.depositsOfByToken(owner.address, token1.address)).to.equal(5);
-    expect(await stakeVault.depositsOf(owner.address)).to.equal(5);
+    expect(await stakeVault.depositsOfByToken(poolId1, owner.address, token1.address)).to.equal(5);
+    expect(await stakeVault.depositsOf(poolId1, owner.address)).to.equal(5);
     expect(await stakeVault.totalDeposited()).to.equal(5);
 
     expect(await token1.balanceOf(owner.address)).to.equal(95);
   });
 
   it("Stake all tokens and get amount staked", async function() {
-    await stakeVault.depositStake(token1.address, 10, owner.address);
-    await stakeVault.depositStake(token2.address, 10, owner.address);
-    await stakeVault.depositStake(token3.address, 10, owner.address);
+    await stakeVault.depositStake(poolId1, token1.address, 10, owner.address);
+    await stakeVault.depositStake(poolId1, token2.address, 10, owner.address);
+    await stakeVault.depositStake(poolId1, token3.address, 10, owner.address);
 
     expect(await token1.balanceOf(owner.address)).to.equal(90);
     expect(await token2.balanceOf(owner.address)).to.equal(90);
     expect(await token3.balanceOf(owner.address)).to.equal(90);
 
-    expect(await stakeVault.depositsOf(owner.address)).to.equal(30);
+    expect(await stakeVault.depositsOf(poolId1, owner.address)).to.equal(30);
     expect(await stakeVault.totalDeposited()).to.equal(30);
   });
 
   it("Stake multiple tokens, multiple users", async function() {
     _mintTokensAndApprove(token2, acc1, 100);
 
-    await stakeVault.depositStake(token1.address, 10, owner.address);
-    await stakeVault.depositStake(token2.address, 10, owner.address);
+    await stakeVault.depositStake(poolId1, token1.address, 10, owner.address);
+    await stakeVault.depositStake(poolId1, token2.address, 10, owner.address);
 
     // Deposit from another account
-    await stakeVault.depositStake(token1.address, 10, acc1.address);
-    await stakeVault.depositStake(token2.address, 10, acc1.address);
+    await stakeVault.depositStake(poolId1, token1.address, 10, acc1.address);
+    await stakeVault.depositStake(poolId1, token2.address, 10, acc1.address);
 
     expect(await token1.balanceOf(owner.address)).to.equal(90);
     expect(await token2.balanceOf(owner.address)).to.equal(90);
     expect(await token1.balanceOf(acc1.address)).to.equal(90);
     expect(await token2.balanceOf(acc1.address)).to.equal(90);
 
-    expect(await stakeVault.depositsOf(owner.address)).to.equal(20);
-    expect(await stakeVault.depositsOf(acc1.address)).to.equal(20);
+    expect(await stakeVault.depositsOf(poolId1, owner.address)).to.equal(20);
+    expect(await stakeVault.depositsOf(poolId1, acc1.address)).to.equal(20);
     expect(await stakeVault.totalDeposited()).to.equal(40);
   });
 
   it("Withdraw with wrong account", async function() {
-    await stakeVault.depositStake(token1.address, 10, owner.address);
-    await stakeVault.depositStake(token2.address, 10, owner.address);
-    await stakeVault.depositStake(token3.address, 10, owner.address);
+    await stakeVault.depositStake(poolId1, token1.address, 10, owner.address);
+    await stakeVault.depositStake(poolId1, token2.address, 10, owner.address);
+    await stakeVault.depositStake(poolId1, token3.address, 10, owner.address);
 
-    await expect(stakeVault.connect(acc1).encumber(owner.address, token1.address, 10))
+    await expect(stakeVault.connect(acc1).encumber(poolId1, owner.address, token1.address, 10))
       .to.be.reverted;
-    await expect(stakeVault.connect(acc1).withdraw()).to.be.reverted;
+    await expect(stakeVault.connect(acc1).withdraw(poolId1)).to.be.reverted;
   });
 
   it("Stake multiple tokens by one account then withdraw", async function() {
-    await stakeVault.depositStake(token1.address, 10, owner.address);
-    await stakeVault.depositStake(token2.address, 10, owner.address);
-    await stakeVault.depositStake(token3.address, 10, owner.address);
+    await stakeVault.depositStake(poolId1, token1.address, 10, owner.address);
+    await stakeVault.depositStake(poolId1, token2.address, 10, owner.address);
+    await stakeVault.depositStake(poolId1, token3.address, 10, owner.address);
 
-    await stakeVault.encumber(owner.address, token1.address, 10);
-    await stakeVault.encumber(owner.address, token2.address, 10);
-    await stakeVault.encumber(owner.address, token3.address, 10);
+    await stakeVault.encumber(poolId1, owner.address, token1.address, 10);
+    await stakeVault.encumber(poolId1, owner.address, token2.address, 10);
+    await stakeVault.encumber(poolId1, owner.address, token3.address, 10);
 
-    await stakeVault.withdraw();
+    await stakeVault.withdraw(poolId1);
 
     expect(await token1.balanceOf(recvFunds.address)).to.equal(10);
     expect(await token2.balanceOf(recvFunds.address)).to.equal(10);
@@ -135,18 +142,18 @@ describe("Test stake vault", function() {
   });
 
   it("Stake multiple accounts then withdraw", async function() {
-    await stakeVault.depositStake(token1.address, 10, acc1.address);
-    await stakeVault.depositStake(token1.address, 10, acc1.address);
-    await stakeVault.depositStake(token1.address, 10, acc2.address);
-    await stakeVault.depositStake(token1.address, 10, acc2.address);
-    await stakeVault.depositStake(token1.address, 10, owner.address);
-    await stakeVault.depositStake(token1.address, 10, owner.address);
+    await stakeVault.depositStake(poolId1, token1.address, 10, acc1.address);
+    await stakeVault.depositStake(poolId1, token1.address, 10, acc1.address);
+    await stakeVault.depositStake(poolId1, token1.address, 10, acc2.address);
+    await stakeVault.depositStake(poolId1, token1.address, 10, acc2.address);
+    await stakeVault.depositStake(poolId1, token1.address, 10, owner.address);
+    await stakeVault.depositStake(poolId1, token1.address, 10, owner.address);
 
-    await stakeVault.encumber(acc1.address, token1.address, 10);
-    await stakeVault.encumber(acc2.address, token1.address, 10);
-    await stakeVault.encumber(owner.address, token1.address, 10);
+    await stakeVault.encumber(poolId1, acc1.address, token1.address, 10);
+    await stakeVault.encumber(poolId1, acc2.address, token1.address, 10);
+    await stakeVault.encumber(poolId1, owner.address, token1.address, 10);
 
-    await stakeVault.withdraw();
+    await stakeVault.withdraw(poolId1);
 
     expect(await token1.balanceOf(recvFunds.address)).to.equal(30);
     expect(await token2.balanceOf(recvFunds.address)).to.equal(0);

8a1478f75dc822d95a1c6e6a92a353fc350e8b6b
José Petry <jose26petry@gmail.com>
2021-06-01 13:58:01 -0300

    updated StakeVault

--- contracts/new_StakeVault.sol
+++ contracts/new_StakeVault.sol
@@ -15,4 +15,16 @@ contract StakeVault is Ownable {
         bool isCommitted;
     }
 
+    address private _poolTrackerContract;
+    mapping(uint256 => Stake) _stakes;
+    mapping(address => uint256[]) _stakesByAccount; // holds an array of stakes for one investor. Each element of the array is an ID for the _stakes array
+
+    struct PoolInfo {
+        address sponsor;
+        PoolStatus status;
+        uint256 expiration;
+    }
+
+    mapping(uint256 => PoolInfo) poolsById;
+
 }

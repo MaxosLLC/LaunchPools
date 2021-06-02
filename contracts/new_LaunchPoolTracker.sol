a380e008387425f2c4f4f018ea3692c2080748d9
José Petry <jose26petry@gmail.com>
2021-06-02 10:11:52 -0300

    added functions comments

--- contracts/new_LaunchPoolTracker.sol
+++ contracts/new_LaunchPoolTracker.sol
@@ -31,20 +31,32 @@ contract LaunchPoolTracker is Ownable {
 
     }
 
+    // called from the stakeVault. Adds to a list of the stakes in a pool, in stake order
     function addStake (uint256 stakeId) public {}
 
+    // Get a list of stakes for the pool. This will be used by users, and also by the stakeVault
+    // returns a list of IDs (figure out how to identify stakes in the stakevault. We know the pool)
     function getStakes (uint256 poolId) public {}
     
+    // Put in committing status. Save a link to the offer
+    // url contains the site that the description of the offer made by the sponsor
     function newOffer (uint256 poolId, string url) public {}
     
+    // put back in staking status.
     function cancelOffer (uint256 poolId) public {}
     
+    // runs the logic for an offer that fails to reach minimum commitment, or succeeds and goes to Delivering status
     function endOffer (uint256 poolId) public {}
     
+    // OPTIONAL IN THIS VERSION. calculates new dollar values for stakes. 
+    // Eventually, we will save these values at the point were we go to “deliver” the investment amount based on the dollar value of a committed stake.
     function setValues () public {}
     
+    // OPTIONAL IN THIS VERSION. We need a way to report the list of committed stakes, with the value of the committed stakes and the investor. 
+    //This forms a list of the investments that need to be delivered. It is basically a “setValues” followed by getStakes.
     function getInvestmentValues () public {}
     
+    // calls stakeVault closePool, sets status to closed
     function close (uint256 poolId) public {}
 
 }

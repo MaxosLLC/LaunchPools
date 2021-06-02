4db661dfcdc2eddabfd3b3aa6ccbd6859ebd00d9
Software Engineer <68395156+SoftwareEngineer1014@users.noreply.github.com>
2021-06-03 02:40:39 +0300

    Fix issues in addStake, unStake function and compile without error

--- contracts/new_StakeVault.sol
+++ contracts/new_StakeVault.sol
@@ -2,14 +2,14 @@
 pragma solidity 0.8.4;
 
 import "./interfaces/IERC20Minimal.sol";
+import "./new_LaunchPoolTracker.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
 contract StakeVault is Ownable {
-    enum PoolStatus {AcceptingStakes, AcceptingCommitments, Funded, Closed}
+    enum PoolStatus { AcceptingStakes, AcceptingCommitments, Funded, Closed}
 
-    struct Stake {
-        
+    struct Stake {        
         uint256 id;
         address staker;
         address token;
@@ -18,7 +18,7 @@ contract StakeVault is Ownable {
         bool isCommitted;
     }
 
-    address private _poolTrackerContract;
+    LaunchPoolTracker private _poolTrackerContract;
     uint256 private _curStakeId;
     mapping(uint256 => Stake) public _stakes;
     mapping(address => uint256[]) public _stakesByAccount; // holds an array of stakes for one investor. Each element of the array is an ID for the _stakes array
@@ -31,47 +31,6 @@ contract StakeVault is Ownable {
 
     mapping(uint256 => PoolInfo) poolsById;
 
-    function _atStage(uint256 poolId, PoolStatus stage_) private view returns (bool) {        
-        LaunchPool storage lp = _poolTrackerContract.poolsById[poolId];
-        return lp.stage == stage_;
-    }
-
-    modifier isTokenAllowed(address _tokenAddr) {
-        require(
-            _poolTrackerContract._allowedTokenAddresses[_tokenAddr],
-            "Cannot deposit that token"
-        );
-        _;
-    }
-
-    modifier isPoolOpen(uint256 poolId) {
-        LaunchPool storage lp = _poolTrackerContract.poolsById[poolId];
-
-        if (block.timestamp > lp.poolExpiry.startTime + lp.poolExpiry.duration) {
-            lp.stage = PoolStatus.Closed;
-        }
-
-        require(!_atStage(poolId, PoolStatus.Closed), "LaunchPool is closed");
-        _;
-    }
-
-    modifier canStake(uint256 poolId) {
-        require(
-            _atStage(poolId, PoolStatus.AcceptingStakes) ||
-                _atStage(poolId, PoolStatus.AcceptingCommitments),
-            "Not in desired stage"
-        );
-        _;
-    }
-
-    modifier canCommit(uint256 poolId) {
-        require(
-            _atStage(poolId, PoolStatus.AcceptingCommitments),
-            "Not in either of desired stages"
-        );
-        _;
-    }
-
     modifier senderOwnsStake(uint256 stakeId) {
         Stake memory st = _stakes[stakeId];
         require(
@@ -80,18 +39,6 @@ contract StakeVault is Ownable {
         );
         _;
     }
-
-    modifier isOfferOpen(uint256 poolId) {
-        LaunchPool storage lp = _poolTrackerContract.poolsById[poolId];
-
-        require(
-            (block.timestamp <= lp.offerExpiry.startTime + lp.offerExpiry.duration) &&
-                _atStage(poolId, PoolStatus.AcceptingCommitments),
-            "Offer is closed"
-        );
-        _;
-    }
-
 
     // @notice Add PoolInfo in the position of poolId
     function addPool (uint256 poolId, address sponsor, uint256 expiration) public {
@@ -104,9 +51,6 @@ contract StakeVault is Ownable {
     
     // @notice Add Stake 
     function addStake (uint256 poolId, address token, uint256 amount) public
-        isPoolOpen(poolId)
-        isTokenAllowed(token)
-        canStake(poolId) 
     {
         address staker = msg.sender;
         uint256 _currStakeId = ++_curStakeId;
@@ -121,7 +65,7 @@ contract StakeVault is Ownable {
 
         _stakesByAccount[staker].push(_currStakeId);
 
-        _poolTrackerContract.addStake(poolId, _currStakeId);
+        _poolTrackerContract.addStake(_currStakeId);
 
         // If the transfer fails, we revert and don't record the amount.
         require(
@@ -132,59 +76,30 @@ contract StakeVault is Ownable {
     
     // @notice Un-Stake
     function unStake (uint256 stakeId) public 
-        isPoolOpen(poolId)
-        senderOwnsStake(poolId, stakeId)
-        canStake(poolId) 
+        senderOwnsStake(stakeId)
     {
         require(!_stakes[stakeId].isCommitted, "cannot unstake commited stake");
         
-        Stake storage st = _stakes[stakeId];
-        address staker = st.staker;
-
-        delete _stakes[stakeId];
-
-        // @notice find Stake in staker's stake list
-        uint256 length = _stakesByAccount[staker].length;
-        uint256 stakeIdx = -1;
-        for(uint256 i = 0 ; i < length ; i ++) {
-            if(_stakesByAccount[staker][i] == stakeId) {
-                stakeIdx = int256(i);
-                break;
-            }
-        }
-        assert(stakeIdx != -1);
-
-        // @notice remove Stake in staker's stake list
-        uint256[] memory accountStakes = _stakesByAccount[staker];
-        uint256 lastIdx = accountStakes.length - 1;
-        if(stakeIdx != lastIdx) {
-            _stakesByAccount[staker][stakeIdx] = _stakesByAccount[staker][lastIdx];
-        }
-        _stakesByAccount[staker].pop();
-
         // @notice withdraw Stake
         require(
-            IERC20Minimal(st.token).transfer(st.staker, st.amount), "Could not send the moneys"
+            IERC20Minimal(_stakes[stakeId].token).transfer( _stakes[stakeId].staker,  _stakes[stakeId].amount), "Could not send the moneys"
         );
+
+        _stakes[stakeId].amount = 0;
     }
     
     function commitStake (uint256 stakeId) public 
-        isPoolOpen(poolId)
-        isOfferOpen(poolId)
-        canCommit(poolId)
-        senderOwnsStake(poolId, stakeId)
+        senderOwnsStake(stakeId)
     {
         require(!_stakes[stakeId].isCommitted, "Stake is already committed");
-        require(stakes[stakeId].staker != address(0), "Stake doesn't exist");
+        require(_stakes[stakeId].staker != address(0), "Stake doesn't exist");
 
         _stakes[stakeId].isCommitted = true;
     }
     
     function unCommitStakes (uint256 poolId) public 
-        isPoolOpen(poolId)
     {
-        uint256 length = _stakes.length;
-        for(uint256 i = 0 ; i < length ; i ++) {
+        for(uint256 i = 0 ; i < _curStakeId ; i ++) {
             if(_stakes[i].poolId == poolId){
                 _stakes[i].isCommitted = false;
             }

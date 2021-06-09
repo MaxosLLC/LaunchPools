ab05fdf624087e62d6e1a1fbc8fc08aa4d3f38e9
Software Engineer <84344255+prodevghost@users.noreply.github.com>
2021-06-09 19:18:44 +0300

    Add constructor and setPoolContract()

--- contracts/new_StakeVault.sol
+++ contracts/new_StakeVault.sol
@@ -32,6 +32,15 @@ contract StakeVault is Ownable {
 
     mapping(uint256 => PoolInfo) poolsById;
     
+    // @notice create a new Stakevault
+    constructor(LaunchPoolTracker poolTrackerContract_) {
+        _poolTrackerContract = poolTrackerContract_;
+    }
+
+    function public setPoolContract(LaunchPoolTracker poolTrackerContract_) {
+        _poolTrackerContract = poolTrackerContract_;
+    }
+
     // Called  by a launchPool. Adds to the poolsById mapping in the stakeVault. Passes the id from the poolIds array.
     // Sets the sponsor and the expiration date and sets the status to “Staking”
     // The sponsor becomes the owner
